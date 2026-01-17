59aa8ca328a4e1e1f3a289edbff134dbb6c8e741
lis24005 <lis24005@byui.edu>
2026-01-17 15:36:49 -0700

    Add files via upload

---
+++ activity_one.cs
@@ -0,0 +1,9 @@
+
+Console.Write("What is your first name: ");
+string first_name = Console.ReadLine();
+
+Console.Write("What is your last name: ");
+string last_name = Console.ReadLine();
+
+Console.WriteLine($"\nYour name is {last_name}, {first_name} {last_name}.");
+
